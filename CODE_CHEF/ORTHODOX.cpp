edbb38bda18a04c2e501e4c134c8595c79ea7614
akash <akash@akashs-MacBook-Pro.local>
2020-07-21 19:19:14 +0530

    Code Chef July CookOff_1

--- CODE_CHEF/ORTHODOX.cpp
+++ CODE_CHEF/ORTHODOX.cpp
@@ -1,60 +1,89 @@
-#include<iostream>
 #include<bits/stdc++.h>
-#include<unordered_set>
-#include<unordered_map>
 using namespace std;
-int
-main ()
-{
-  int t;
-  cin >> t;
-  while (t--)
-    {
-      int n;
-      cin >> n;
-      bool flag = true;
-
-      int a[n];
-      for (int i = 0; i < n; i++)
-	{
-	  cin >> a[i];
-	}
-      if (n > 60)
-	{
-	  cout << "NO" << endl;
-	  flag = false;
-	}
-
-      if (flag == true)
-	{
-	  unordered_set < int >se;
-	  for (int i = 0; i < n; i++)
-	    {
-	      long long curr = 0;
-	      for (int j = i; j < n; j++)
-		{
-		  curr |= a[j];
-		  if (se.find (curr) != se.end ())
-		    {
-		      cout << "NO" << endl;
-		      flag = false;
-		      break;
-		    }
-		  se.insert (curr);
-
-		}
-	      if (flag == false)
-		{
-		  break;
-		}
-
-	    }
-	  if (flag == true)
-	    cout << "YES" << endl;
-
-
-	}
+ 
+const int N = 1e5 + 9;
+const long long M = 1e18;
+ 
+long long a[N];
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t; cin >> t;
+    assert(1 <= t && t <= 300);
+    int sum = 0;
+    while (t--) {
+        int n; cin >> n;
+        assert(1 <= n && n <= 100000);
+        sum += n;
+        for (int i = 1; i <= n; i++) {
+            cin >> a[i];
+            assert(0 <= a[i] && a[i] <= M);
+        }
+        if (n > 60) {
+            cout << "NO\n";
+            continue;
+        }
+        set<long long> se;
+        bool ok = true;
+        for (int i = 1; i <= n; i++) {
+            long long cur = 0;
+            for (int j = i; j <= n; j++) {
+                cur |= a[j];
+                if (se.find(cur) != se.end()) {
+                    ok = false;
+                    break;
+                }
+                se.insert(cur);
+            }
+            if (!ok) break;
+        }
+        if (ok) cout << "YES\n";
+        else cout << "NO\n";
     }
-
-  return 0;
-}
+    assert(1 <= sum && sum <= 300000);
+    return 0;
+} #include<bits/stdc++.h>
+using namespace std;
+ 
+const int N = 1e5 + 9;
+const long long M = 1e18;
+ 
+long long a[N];
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t; cin >> t;
+    assert(1 <= t && t <= 300);
+    int sum = 0;
+    while (t--) {
+        int n; cin >> n;
+        assert(1 <= n && n <= 100000);
+        sum += n;
+        for (int i = 1; i <= n; i++) {
+            cin >> a[i];
+            assert(0 <= a[i] && a[i] <= M);
+        }
+        if (n > 60) {
+            cout << "NO\n";
+            continue;
+        }
+        set<long long> se;
+        bool ok = true;
+        for (int i = 1; i <= n; i++) {
+            long long cur = 0;
+            for (int j = i; j <= n; j++) {
+                cur |= a[j];
+                if (se.find(cur) != se.end()) {
+                    ok = false;
+                    break;
+                }
+                se.insert(cur);
+            }
+            if (!ok) break;
+        }
+        if (ok) cout << "YES\n";
+        else cout << "NO\n";
+    }
+    assert(1 <= sum && sum <= 300000);
+    return 0;
+} 
