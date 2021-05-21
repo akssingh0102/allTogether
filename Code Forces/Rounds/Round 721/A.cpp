b02b00e97cc22e3e5f08ea057adde52694d20769
Akash <akash@Akashs-MacBook-Pro.local>
2021-05-21 18:59:50 +0530

    Contest

---
+++ Code Forces/Rounds/Round 721/A.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<unordered_set>
+#include<unordered_map>
+using namespace std;
+
+#define ll                  long long int 
+#define llmin               -1e18
+#define ff                  first
+#define ss                  second
+#define pb                  push_back
+#define vi                  vector<int>
+#define take(a,n)           vector <int> a; for(int i=0;i<n;i++){int aa; cin>>aa; a.pb(aa);}
+#define vll                 vector<ll> 
+#define full(a)             (a.begin(),a.end())
+#define vc                  vector<char>
+#define iz(n)               int n; cin>>n;
+#define iz2(n,m)             int n,m; cin>>n>>m;
+#define mii                 map<int,int>
+#define setbits(x)          __builtin_popcountll(x)
+#define zerobits(x)         __builtin_ctzll(x)    //zeros before first 1
+#define com                 int t; cin>>t; while(t--)
+#define forn(n)             for(int i=0;i<n;i++)
+#define fo(x,y)             for(int i=x;i<y;i++)
+#define pq                  priority_queue <int, vector<int>, greater<int> >
+#define mod                 1000000007
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    com{
+        iz(n)
+        int prev,curr;
+        prev=2;
+        if(n==1 || n==2){
+            cout<<n-1<<endl;
+        }
+        else{
+            prev=2;
+            curr=2;
+            while(curr<=n){
+                prev=curr;
+                curr*=2;
+            }
+            cout<<prev-1<<endl;
+        }
+    }
+    return 0;
+}
--- Data Structures/Graphs/Self Practice/Graph.cpp
+++ Data Structures/Graphs/Self Practice/Graph.cpp
@@ -170,6 +170,34 @@ class Graph{
         
     }
 
+//******************************** Topological Sort ****************************
+
+    void topologicalSort() 
+{ 
+    vector<int> in_degree(V, 0); 
+  
+    for (int u = 0; u < V; u++) { 
+        for (int x:adj[u]) 
+            in_degree[x]++; 
+    } 
+  
+    queue<int> q; 
+    for (int i = 0; i < V; i++) 
+        if (in_degree[i] == 0) 
+            q.push(i); 
+
+  
+    while (!q.empty()) { 
+        int u = q.front(); 
+        q.pop(); 
+        cout<<u<<" "; 
+  
+        for (int x: adj[u]) 
+            if (--in_degree[x] == 0) 
+                q.push(x); 
+    } 
+}
+
 };
 
 
